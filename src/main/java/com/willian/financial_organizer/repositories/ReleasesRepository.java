cf5f9225594562a953757f9113218075860f8446
Willian Costa <willian95@outlook.com>
2024-05-27 13:26:29 -0300

    feat: add ReleasesRepository

--- src/main/java/com/willian/financial_organizer/repositories/ReleasesRepository.java
+++ src/main/java/com/willian/financial_organizer/repositories/ReleasesRepository.java
@@ -1,9 +1,9 @@
 package com.willian.financial_organizer.repositories;
 
-import com.willian.financial_organizer.model.User;
+import com.willian.financial_organizer.model.Releases;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface ReleasesRepository extends JpaRepository<Releases, Long> {
 }
