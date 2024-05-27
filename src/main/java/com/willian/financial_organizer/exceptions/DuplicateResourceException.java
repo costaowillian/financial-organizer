c042dc1bf206e151826490a3f8d48bc57086634e
Willian Costa <willian95@outlook.com>
2024-05-27 15:49:52 -0300

    feat: add InvalidJwtAuthenticationException

--- src/main/java/com/willian/financial_organizer/exceptions/DuplicateResourceException.java
+++ src/main/java/com/willian/financial_organizer/exceptions/DuplicateResourceException.java
@@ -1,5 +1,9 @@
 package com.willian.financial_organizer.exceptions;
 
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
 public class DuplicateResourceException extends RuntimeException {
     public DuplicateResourceException(String s) {
         super(s);
--- src/main/java/com/willian/financial_organizer/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/willian/financial_organizer/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -2,6 +2,7 @@ package com.willian.financial_organizer.exceptions.handler;
 
 import com.willian.financial_organizer.exceptions.DuplicateResourceException;
 import com.willian.financial_organizer.exceptions.ExceptionResponse;
+import com.willian.financial_organizer.exceptions.InvalidJwtAuthenticationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -27,5 +28,11 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
     public final ResponseEntity<ExceptionResponse> handleDuplicateResourceException(Exception ex, WebRequest request) {
         ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(exceptionResponse, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(InvalidJwtAuthenticationException.class)
+    public final ResponseEntity<ExceptionResponse> handleInvalidJwtAuthenticationException(Exception ex, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.FORBIDDEN);
     }
 }
