ca810e4e7fa60e6d2562cd7b1c373a563080a008
Willian Costa <willian95@outlook.com>
2024-05-27 10:29:00 -0300

    build: create application springBoot

---
+++ src/main/java/com/willian/financial_organizer/FinancialOrganizerApplication.java
@@ -0,0 +1,13 @@
+package com.willian.financial_organizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialOrganizerApplication.class, args);
+	}
+
+}
