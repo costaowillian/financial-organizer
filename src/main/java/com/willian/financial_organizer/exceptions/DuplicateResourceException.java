12ab4f26565abe38b9407745baefe1ebd8bcfe0b
Willian Costa <willian95@outlook.com>
2024-05-27 14:34:37 -0300

    feat: add DuplicateResourceException

---
+++ src/main/java/com/willian/financial_organizer/exceptions/DuplicateResourceException.java
@@ -0,0 +1,7 @@
+package com.willian.financial_organizer.exceptions;
+
+public class DuplicateResourceException extends RuntimeException {
+    public DuplicateResourceException(String s) {
+        super(s);
+    }
+}
