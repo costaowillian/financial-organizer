602d0b5cc7620e547b656320c15716cc29014368
Willian Costa <willian95@outlook.com>
2024-05-28 15:09:56 -0300

    feat: implements method update on Releases services

--- src/main/java/com/willian/financial_organizer/services/ReleasesServices.java
+++ src/main/java/com/willian/financial_organizer/services/ReleasesServices.java
@@ -1,13 +1,17 @@
 package com.willian.financial_organizer.services;
 
+import com.willian.financial_organizer.exceptions.RequiredObjectIsNullException;
+import com.willian.financial_organizer.exceptions.ResourceNotFoundException;
 import com.willian.financial_organizer.model.Releases;
 import com.willian.financial_organizer.model.enums.ReleasesStatus;
 import com.willian.financial_organizer.repositories.ReleasesRepository;
 import com.willian.financial_organizer.services.interfaces.IReleasesServices;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class ReleasesServices  implements IReleasesServices {
@@ -16,13 +20,20 @@ public class ReleasesServices  implements IReleasesServices {
     private ReleasesRepository repository;
 
     @Override
+    @Transactional
     public Releases save(Releases releases) {
-        return null;
+        return repository.save(releases);
     }
 
     @Override
+    @Transactional
     public Releases update(Releases releases) {
-        return null;
+        if(releases == null) throw new RequiredObjectIsNullException();
+
+        Releases entity = repository.findById(releases.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+
+        return repository.save(releases);
     }
 
     @Override
