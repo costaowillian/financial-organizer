b3425cf7549802f4167911260bce9c065a9ed753
Willian Costa <willian95@outlook.com>
2024-05-28 09:47:42 -0300

    refactor: passwordEncoder on user Services

--- src/main/java/com/willian/financial_organizer/services/UserServices.java
+++ src/main/java/com/willian/financial_organizer/services/UserServices.java
@@ -7,17 +7,19 @@ import com.willian.financial_organizer.repositories.UserRepository;
 import com.willian.financial_organizer.services.interfaces.IUserServices;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class UserServices implements IUserServices {
 
     @Autowired
     private UserRepository repository;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
 
     @Override
     @Transactional
@@ -43,6 +45,18 @@ public class UserServices implements IUserServices {
 
     @Override
     public String passwordEncoder(String password) {
-        return passwordEncoder.encode(password);
+        Map<String, PasswordEncoder> encoderMap = new HashMap<>();
+
+        Pbkdf2PasswordEncoder pbkdf2PasswordEncoder = new Pbkdf2PasswordEncoder("", 8, 185000,
+                Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
+
+        encoderMap.put("pbkdf2", pbkdf2PasswordEncoder);
+
+        DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoderMap);
+
+        passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2PasswordEncoder);
+        String result = passwordEncoder.encode(password);
+
+        return result.substring("{pbkdf2}".length());
     }
 }
