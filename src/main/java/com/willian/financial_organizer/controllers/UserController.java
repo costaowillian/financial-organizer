0724d736070aa11d50e8bfbdff81861d111bdd4a
Willian Costa <willian95@outlook.com>
2024-05-28 11:45:31 -0300

    feat: add anotations to user controller

--- src/main/java/com/willian/financial_organizer/controllers/UserController.java
+++ src/main/java/com/willian/financial_organizer/controllers/UserController.java
@@ -5,17 +5,23 @@ import com.willian.financial_organizer.dtos.CreateUserDTO;
 import com.willian.financial_organizer.dtos.UserResponseDTO;
 import com.willian.financial_organizer.services.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 
-
+@RestController
+@RequestMapping("/api/v1/user")
 public class UserController implements IUserController {
     @Autowired
     private UserServices services;
 
     @Override
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<UserResponseDTO> create(CreateUserDTO userDTO) {
         UserResponseDTO user = services.createUser(userDTO);
 
