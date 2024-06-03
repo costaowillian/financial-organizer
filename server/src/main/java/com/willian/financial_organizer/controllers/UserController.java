09799a4aed17343c049289c32312eff500cfdf73
Willian Costa <willian95@outlook.com>
2024-06-03 16:05:16 -0300

    fix: User controller

--- server/src/main/java/com/willian/financial_organizer/controllers/UserController.java
+++ server/src/main/java/com/willian/financial_organizer/controllers/UserController.java
@@ -5,14 +5,12 @@ import com.willian.financial_organizer.services.ReleasesServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 
 @RestController
+@RequestMapping("/api/v1/user")
 public class UserController {
 
     @Autowired
