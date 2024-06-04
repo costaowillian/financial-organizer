daac70a860f37fee14db23e323490e9746c0422d
Willian Costa <willian95@outlook.com>
2024-06-04 10:37:04 -0300

    feat: implementes method register in authservice

--- client/my-finances/src/services/authServices.js
+++ client/my-finances/src/services/authServices.js
@@ -21,6 +21,16 @@ const authService = {
             return false;
         }
     },
+
+    cadastrar: async (credentials) => {
+      try {
+        const response = await api.post('/auth/signup', credentials);
+
+        return response.data;
+      } catch (error) {
+        throw error;
+      }
+    },
 }
 
 export default authService;
