e5d4b8e460c9c5266ae91370f459fc3f2d9b5d09
Willian Costa <willian95@outlook.com>
2024-06-04 09:31:25 -0300

    feat: implements axios services

--- client/my-finances/src/services/api.js
+++ client/my-finances/src/services/api.js
@@ -3,11 +3,23 @@ import axios from "axios";
 const api = axios.create (
     {
         baseURL: 'http://localhost:80',
+        timeout: 10000,
         headers: {
             'Content-Type': 'application/json', 
             'Accept': 'application/json',
         }
     }
 )
+
+api.interceptors.request.use(
+    config => {
+      const userData = localStorage.getItem('user_data');
+      if (token) {
+        config.headers.Authorization = `Bearer ${userData.accesToken}`;
+      }
+      return config;
+    },
+    error => Promise.reject(error)
+);
 
 export default api;
