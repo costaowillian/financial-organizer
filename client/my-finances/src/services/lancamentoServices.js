4af381eb1f053b7ecfb304b3717edda3c5198f89
Willian Costa <willian95@outlook.com>
2024-06-05 14:15:23 -0300

    feat: add request to create release at releases view

--- client/my-finances/src/services/lancamentoServices.js
+++ client/my-finances/src/services/lancamentoServices.js
@@ -2,8 +2,8 @@ import api from './api';
 
 const lancamentoService = {
     get: (url) => api.get(url),
-    post: (data) => api.post(data),
-    patch: (data) => api.patch(data),
+    post: (url, data) => api.post(url, data),
+    patch: (url,  data) => api.patch(url, data),
     delete: (url) => api.delete(url),
     put: (url) => api.put(url)
 }
--- client/my-finances/src/views/lancamentos/cadastroLancamento.js
+++ client/my-finances/src/views/lancamentos/cadastroLancamento.js
@@ -3,6 +3,8 @@ import { useNavigate } from 'react-router-dom';
 import Card from '../../components/card';
 import FormGroup from '../../components/form-group';
 import SelectMenu from '../../components/selectMenu';
+import lancamentoService from '../../services/lancamentoServices';
+import { mensagemError, mensagemSucesso } from "../../components/toastr";
 
 const CadastroLancamento = () => {
 
@@ -12,6 +14,7 @@ const CadastroLancamento = () => {
     const [ano, setAno] = useState();
     const [mes, setMes] = useState();
     const [valor, setValor] = useState();
+    const [tipo, setTipo] = useState('');
     const [status, setStatus] = useState('');
 
     const opcoesTipo = [
@@ -34,13 +37,41 @@ const CadastroLancamento = () => {
         { label: 'Dezembro', value: '12' },
     ];
 
-    const definiTipo = (selectedValue) => {
-        setStatus(selectedValue);
+    const defineTipo = (selectedValue) => {
+        setTipo(selectedValue);
     };
 
-    const definiMes = (selectedValue) => {
+    const defineMes = (selectedValue) => {
         setMes(selectedValue);
     };
+
+    const cadastrar = async (event) => {
+        event.preventDefault();
+
+        const userData = JSON.parse(localStorage.getItem("user_data"));
+
+        let data = JSON.stringify({
+            "description": descricao,
+            "value": valor,
+            "type": tipo,
+            "user_id": userData.userId,
+            "registration_date": new Date(),
+            "year": ano,
+            "month": mes
+        });
+
+        try {
+            const response = await lancamentoService.post("/api/v1/releases", data);
+            if (response) {
+                mensagemSucesso("Lançamento cadastrado com sucesso!")
+                navigate("/lancamentos");
+            }
+        } catch (e) {
+            if (e.response || e.status == 500) {
+                mensagemError("Falha ao cadastrar, por favor tente novamente!");
+            }
+        }
+    }
 
     return (
         <>
@@ -48,7 +79,7 @@ const CadastroLancamento = () => {
                 <div className="row">
                     <div className="col-lg-12">
                         <div className="bs-component">
-                            <form >
+                            <form onSubmit={cadastrar}>
                                 <fieldset>
                                     <FormGroup label="Descrição: *" htmlFor="descricao" >
                                         <input type="text"
@@ -69,7 +100,7 @@ const CadastroLancamento = () => {
                                         </div>
                                         <div className='col-md-6'>
                                             <FormGroup label="Mês: *" htmlFor="mes" >
-                                                <SelectMenu options={opcoesMeses} onChange={definiMes} />
+                                                <SelectMenu options={opcoesMeses} onChange={defineMes} />
                                             </FormGroup>
                                         </div>
                                     </div>
@@ -85,16 +116,16 @@ const CadastroLancamento = () => {
                                         </div>
                                         <div className='col-md-4'>
                                             <FormGroup label="Tipo: *" htmlFor="tipo" >
-                                                <SelectMenu options={opcoesTipo} onChange={definiTipo} />
+                                                <SelectMenu options={opcoesTipo} onChange={defineTipo} />
                                             </FormGroup>
                                         </div>
                                         <div className='col-md-4'>
                                             <FormGroup label="Status: *" htmlFor="status" >
-                                            <input type="status"
+                                                <input type="status"
                                                     value={status}
                                                     onChange={e => setValor(e.target.value)}
                                                     className="form-control" id="status" aria-describedby="emailHelp"
-                                                    placeholder="Status" disabled="true" />
+                                                    placeholder="Status" disabled />
                                             </FormGroup>
                                         </div>
                                     </div>
