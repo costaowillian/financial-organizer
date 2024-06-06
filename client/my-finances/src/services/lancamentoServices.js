5bb656d5d43d0e5fd57e85ac5afd8d4bdcb27f5f
Willian Costa <willian95@outlook.com>
2024-06-06 09:37:41 -0300

    refactor: release service on client

--- client/my-finances/src/services/lancamentoServices.js
+++ client/my-finances/src/services/lancamentoServices.js
@@ -5,7 +5,7 @@ const lancamentoService = {
     post: (url, data) => api.post(url, data),
     patch: (url,  data) => api.patch(url, data),
     delete: (url) => api.delete(url),
-    put: (url) => api.put(url)
+    put: (url, status) => api.put(url, status)
 }
 
 export default lancamentoService;
