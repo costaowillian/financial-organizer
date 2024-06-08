fc4671d0cd7a7dcb2900f3c8c74e030a88adf377
Willian Costa <willian95@outlook.com>
2024-06-08 10:17:24 -0300

    chore: setup for AbstractIntegrationTest

---
+++ server/src/test/java/com/willian/financial_organizer/configs/TestConfigs.java
@@ -0,0 +1,13 @@
+package com.willian.financial_organizer.configs;
+
+public class TestConfigs {
+    public static final int SERVER_PORT = 8888;
+
+    public static final String HEADER_PARAM_ORIGIN = "Origin";
+    public static final String HEADER_PARAM_AUTHORIZATION = "Authorization";
+
+    public static  final String CONTENT_TYPE_JSON = "application/json";
+
+    public static final String ORIGIN_FAIL = "http://willian.com.br";
+    public static final String ORIGIN_SITE = "http://localhost:3000";
+}
